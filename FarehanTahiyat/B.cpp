0296fd694cda27fab1481dd7beccf9b71892eefa
Tanvir <rahidt.rahid6@gmail.com>
2019-03-24 09:33:29 +0600

    Push it in github

---
+++ FarehanTahiyat/B.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        int a,b,s,t,$,replicas;
+        cin>>a>>b>>s>>$;
+        replicas=a+b=s+$;
+}
+return 0;
+}
---
+++ FarehanTahiyat/C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(1)
+    {
+        int n;
+        cin>>n;
+        if  (1 << 10 << n);
+        {
+            printf("0\n");
+        }
+             if(1 >> n >>10);
+         {
+            printf("1\n");
+       }
+    }
+    return 0;
+}
